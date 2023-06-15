361b19e66c858f6b8caf795a7f2660b4c9035a35
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-06-15 16:37:01 +0200

    remove unused packages

--- src/app/model/InstCooccurrenceNode.ts
+++ src/app/model/InstCooccurrenceNode.ts
@@ -1,5 +1,4 @@
 import {SimulationNodeDatum} from "d3-force";
-import {DataCounterNew} from "./DataCounterNew";
 import {DataCounterSelection} from "./DataCounterSelection";
 
 export interface InstCooccurrenceNode<T,U> extends DataCounterSelection<T,U>, SimulationNodeDatum {
