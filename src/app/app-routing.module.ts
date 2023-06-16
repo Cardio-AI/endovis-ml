fc8a3f020cd09bdeaedcd9e0fac54ea2601e5a07
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-06-16 10:30:20 +0200

    remove unused components and classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {TrainTestComponent} from "./train-test/train-test.component";
-import {ModelEvalComponent} from "../model-eval/model-eval/model-eval.component";
 
 const routes: Routes = [
   { path: '', component: TrainTestComponent},
-  // { path: 'train-test', component: TrainTestComponent},
-  // { path: 'model-eval', component: ModelEvalComponent }
 ];
 
 @NgModule({
--- src/app/model/StreamgraphData.ts
+++
@@ -1,6 +0,0 @@
-export interface StreamgraphData {
-  x: string;
-  train: number;
-  validation: number;
-  test: number;
-}
