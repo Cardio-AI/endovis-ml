79c27d89ad7266ce9a16c8deba20e7d81e6d3272
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-07-17 11:30:13 +0200

    remove unused NestedOccurrence interface

--- src/app/model/NestedOccurrence.ts
+++
@@ -1,7 +0,0 @@
-import {Occurrence} from "./Occurrence";
-import {DataCounterNew} from "./DataCounterNew";
-
-export interface NestedOccurrence<T> extends Occurrence {
-  nestedOccurrences: DataCounterNew<T, Occurrence[]>[]
-
-}
