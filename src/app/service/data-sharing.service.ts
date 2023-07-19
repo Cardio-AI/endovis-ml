d8e9f53c615b1ca967dfc82bc789a5a31ef4e7fb
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2023-07-19 15:41:39 +0200

    buffer dataset updates

--- src/app/service/data-sharing.service.ts
+++ src/app/service/data-sharing.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Subject} from "rxjs";
+import {ReplaySubject, Subject} from "rxjs";
 import {SurgeryData} from "../model/SurgeryData";
 import {DataCounterNew} from "../model/DataCounterNew";
 import {Occurrence} from "../model/Occurrence";
@@ -9,7 +9,7 @@ import {Occurrence} from "../model/Occurrence";
 })
 export class DataSharingService {
 
-  private dataset = new Subject<SurgeryData[]>();
+  private dataset = new ReplaySubject<SurgeryData[]>(1);
   private selection = new Subject<DataCounterNew<number, Occurrence[]>[]>()
 
   dataset$ = this.dataset.asObservable();
@@ -25,33 +25,4 @@ export class DataSharingService {
   updateSelection(selection: DataCounterNew<number, Occurrence[]>[]) {
     this.selection.next(selection);
   }
-
-  // fix me: is merging actually necessary?
-  // private mergeSelection(selection: DataCounterNew<number, Occurrence[]>[]){
-  //   let result: DataCounterNew<number, Occurrence[]>[] = [];
-  //
-  //   let spsList = [...new Set(selection.map(e => e.object))];
-  //
-  //   spsList.forEach(spNr => { // for each sp
-  //     let spOcc:Occurrence[] = selection.filter(e => e.object === spNr).flatMap(u => u.value).sort((a,b) => a.start - b.start);
-  //
-  //     let resSpObj: DataCounterNew<number, Occurrence[]> = {object: spNr, value: []}
-  //
-  //     let prevOcc = spOcc[0];
-  //     spOcc.forEach(occ => {
-  //       if(prevOcc.end >= occ.start) { // overlap
-  //          prevOcc = {start: prevOcc.start, end: Math.max(prevOcc.end, occ.end)}
-  //       } else { // no overlap
-  //         resSpObj.value.push(prevOcc);
-  //         prevOcc = occ;
-  //       }
-  //     });
-  //
-  //     // last element
-  //     resSpObj.value.push(prevOcc);
-  //     result.push(resSpObj);
-  //   });
-  //
-  //   return result;
-  // }
 }
